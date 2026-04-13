6440970dee4c13473e9b54accf3f70b1ffdd3fdb
Fernando <f.miranda25d@gmail.com>
2026-04-12 19:46:03 -0500

    Third commit, resaved from loaded version to regular version

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -6,7 +6,6 @@ public class Program {
     public static void main(String[] args) {
 
         double sandwichPrice = 0;
-        boolean isLoaded = false;
 
         Scanner userInput1 = new Scanner(System.in);
         System.out.println("Would you like sandwich size 1 or 2?: ");
@@ -15,36 +14,23 @@ public class Program {
         int sandwichSize = userInput1.nextInt();
 
         Scanner userInput2 = new Scanner(System.in);
-        System.out.println("Would you like your sandwich loaded?: ");
-        String loadPreference = userInput2.nextLine();
+        System.out.println("Enter your age: ");
+        int userAge = userInput2.nextInt();
 
         if (sandwichSize == 1){
             sandwichPrice = 5.45;
         } else if (sandwichSize == 2){
             sandwichPrice = 8.95;
         }
-/*
-        if (loadPreference == "yes") {
-            if (sandwichSize == 1) {
-                sandwichPrice += 1.00;
-        }   else if (sandwichPrice == 2) {
-                sandwichPrice += 1.75;
-            }
-        }
-*/
-        Scanner userInput3 = new Scanner(System.in);
-        System.out.println("Enter your age: ");
-        int userAge = userInput3.nextInt();
 
         if (userAge <= 17){
             sandwichPrice *= 0.9;
-            System.out.println(sandwichPrice);
+            System.out.println(String.format("%.2f", sandwichPrice));
         } else if (userAge >= 65){
             sandwichPrice *= 0.8;
-            System.out.println(sandwichPrice);
+            System.out.println(String.format("%.2f", sandwichPrice));
         } else {
-            System.out.println(sandwichPrice);
+            System.out.println(String.format("%.2f", sandwichPrice));
         }
-
     }
 }
